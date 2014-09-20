f93b4d67c56afa4e133d46635b268c31438f5ca8
Ben Norris <ben@bsn.io>
2014-09-20 09:03:16 -0400

    Added custom object DayXEntry

---
+++ DayX/DayXEntry.h
@@ -0,0 +1,16 @@
+//
+//  DayXEntry.h
+//  DayX
+//
+//  Created by Ben Norris on 9/20/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DayXEntry : NSObject
+
+- (NSDictionary *)entryDictionary;
+- (id)initWithDictionary: (NSDictionary *)dictionary;
+
+@end
---
+++ DayX/DayXEntry.m
@@ -0,0 +1,44 @@
+//
+//  DayXEntry.m
+//  DayX
+//
+//  Created by Ben Norris on 9/20/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import "DayXEntry.h"
+
+#define titleKey @"title"
+#define textKey @"text"
+#define dateKey @"date"
+
+@interface DayXEntry()
+
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *text;
+@property (nonatomic, strong) NSDate *datestamp;
+
+@end
+
+@implementation DayXEntry
+
+- (NSDictionary *)entryDictionary {
+    
+    NSMutableDictionary *mutableDictionary = [NSMutableDictionary new];
+    if (self.title) [mutableDictionary setValue:self.title forKey:titleKey];
+    if (self.text) [mutableDictionary setValue:self.text forKey:textKey];
+    if (self.datestamp) [mutableDictionary setValue:self.datestamp forKey:dateKey];
+    
+    return mutableDictionary;
+}
+
+- (id)initWithDictionary: (NSDictionary *)dictionary {
+    
+    self.title = [dictionary objectForKey:titleKey];
+    self.text = [dictionary objectForKey:textKey];
+    self.datestamp = [dictionary objectForKey:dateKey];
+    
+    return self;
+}
+
+@end
